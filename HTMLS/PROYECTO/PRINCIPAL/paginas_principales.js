252c0a1106475f15e9a2e4f65e1d73b7b34121be
Daniel <a20212591@pucp.edu.pe>
2024-05-16 01:07:40 -0500

    linkeo pagprincipal admin

--- HTMLS/PROYECTO/PRINCIPAL/paginas_principales.js
+++ HTMLS/PROYECTO/PRINCIPAL/paginas_principales.js
@@ -10,3 +10,26 @@ notificationBadge.addEventListener('mouseenter', () => {
 notificationBadge.addEventListener('mouseleave', () => {
   notificationMessage.style.display = 'none';
 });
+
+function mostrarPopupVecino() {
+  Swal.fire({
+      title: 'Listas de Vecinos',
+      text: '¿A qué lista de solicitudes deseas acceder?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#26DADA',
+      cancelButtonColor: '#26DADA',
+      confirmButtonText: 'Solicitudes de acceso',
+      cancelButtonText: 'Solicitudes a coordinador',
+      customClass: {
+        confirmButton: 'boton-sin-borde' // Agregar una clase personalizada al botón de confirmación
+    }
+  }).then((result) => {
+      if (result.isConfirmed) {
+          window.location.href = "../ADMIN/Vecinos/Solicitudes a acceso/tabla_solicitudes.html"; //Cambiar la ubicacion del login de acuerdo a lo necesario
+      }else if (result.dismiss === Swal.DismissReason.cancel) {
+        // Acción a realizar si se cancela el cierre de sesión
+        window.location.href = "../ADMIN/Vecinos/Postulaciones a coordinacion/tabla_postulaciones.html";
+    }
+  });
+}
